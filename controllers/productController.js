43957164dbb517ceede98aeeea1bd477a7d82afe
Derrick <oseiagyemanb@gmail.com>
2024-03-20 22:56:26 +0000

    refactoring code with cloudinary setup

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,8 @@ const Product = require('../models/Product');
 const CustomError = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
 const createProduct = async (req, res) => {
 	req.body.user = req.user.userId;
@@ -50,31 +52,47 @@ const deleteProduct = async (req, res) => {
 	res.status(StatusCodes.OK).json({ msg: 'Success! Product Removed' });
 };
 
+//Local Image upload
+// const uploadImageLocal = async (req, res) => {
+// 	// console.log(req.files);
+// 	if (!req.files) {
+// 		throw new CustomError.BadRequestError('No File Uploaded');
+// 	}
+
+// 	const productImage = req.files.image;
+// 	if (!productImage.mimetype.startsWith('image')) {
+// 		throw new CustomError.BadRequestError('Please Upload Image');
+// 	}
+
+// 	const maxSize = 1024 * 1024;
+
+// 	if (productImage.size > maxSize) {
+// 		throw new CustomError.BadRequestError(
+// 			'Please upload image smaller than 1MB'
+// 		);
+// 	}
+
+// 	const imagePath = path.join(
+// 		__dirname,
+// 		'../public/uploads/' + `${productImage.name}`
+// 	);
+// 	await productImage.mv(imagePath);
+// 	res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
+// };
+
+//cloudinary setup
 const uploadImage = async (req, res) => {
-	// console.log(req.files);
-	if (!req.files) {
-		throw new CustomError.BadRequestError('No File Uploaded');
-	}
-
-	const productImage = req.files.image;
-	if (!productImage.mimetype.startsWith('image')) {
-		throw new CustomError.BadRequestError('Please Upload Image');
-	}
-
-	const maxSize = 1024 * 1024;
-
-	if (productImage.size > maxSize) {
-		throw new CustomError.BadRequestError(
-			'Please upload image smaller than 1MB'
-		);
-	}
-
-	const imagePath = path.join(
-		__dirname,
-		'../public/uploads/' + `${productImage.name}`
+	const result = await cloudinary.uploader.upload(
+		req.files.image.tempFilePath,
+		{
+			use_filename: true,
+			folder: 'api-test',
+		}
 	);
-	await productImage.mv(imagePath);
-	res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
+	fs.unlinkSync(req.files.image.tempFilePath);
+	return res
+		.status(StatusCodes.OK)
+		.json({ image: { src: result.secure_url } });
 };
 
 module.exports = {
